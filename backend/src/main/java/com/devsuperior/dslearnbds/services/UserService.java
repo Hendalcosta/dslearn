9622927d8db6499ad45f4ca87185c0d2937ac15c
Henderson <hendalcosta@gmail.com>
2024-01-30 18:52:36 -0300

    Custom service level authorization

--- backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
@@ -24,9 +24,13 @@ public class UserService implements UserDetailsService{
 	@Autowired
 	private UserRepository repository;
 	
+	@Autowired
+	private AuthService authService;
+	
 	
 	@Transactional(readOnly = true)
 	public UserDTO findById(Long id) {
+		authService.validateSelfOrAdmin(id); //verifica se o id passado é do proprio usuário ou de um admin. neste projeto usuários comuns só podem consultar a si mesmo
 		Optional<User> obj = repository.findById(id);
 		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new UserDTO(entity);
